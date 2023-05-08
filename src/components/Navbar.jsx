a6c4bf44bfd7e892b48b2fe0524884f817fc475f
TimotheGonin <gonin.timothe@gmail.com>
2023-05-08 10:58:53 +0200

    💄  Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/argentBankLogo.png";
+
+const Navbar = () => {
+	return (
+		<nav className="main-nav">
+			<a className="main-nav-logo" href="/">
+				<img
+					className="main-nav-logo-image"
+					src={logo}
+					alt="Argent Bank Logo"
+				/>
+				<h1 className="sr-only">Argent Bank</h1>
+			</a>
+			<div>
+				<a className="main-nav-item" href="/">
+					<i className="fa fa-user-circle"></i> Sign In{" "}
+				</a>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
