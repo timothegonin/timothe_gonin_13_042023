1c0d785c35e282d7678b374c6b118c43ff876d67
TimotheGonin <gonin.timothe@gmail.com>
2023-05-08 14:53:41 +0200

    🚸 add navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
-import logo from "../assets/argentBankLogo.png";
+import { Link } from 'react-router-dom'
+import logo from '../assets/argentBankLogo.png'
 
 const Navbar = () => {
-	return (
-		<nav className="main-nav">
-			<a className="main-nav-logo" href="/">
-				<img
-					className="main-nav-logo-image"
-					src={logo}
-					alt="Argent Bank Logo"
-				/>
-				<h1 className="sr-only">Argent Bank</h1>
-			</a>
-			<div>
-				<a className="main-nav-item" href="/">
-					<i className="fa fa-user-circle"></i> Sign In{" "}
-				</a>
-			</div>
-		</nav>
-	);
-};
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <Link className="main-nav-item" to="/">
+          <i className="fa fa-user-circle"></i> Sign In{' '}
+        </Link>
+      </div>
+    </nav>
+  )
+}
 
-export default Navbar;
+export default Navbar
