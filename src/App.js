f3ce015b7642a85db15e39524cdb1c9bbfca7294
TimotheGonin <gonin.timothe@gmail.com>
2023-05-08 11:52:38 +0200

    💄 Footer component with actual year

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<React.Fragment>
 			<Navbar />
 			<Home />
+			<Footer />
 		</React.Fragment>
 	);
 }
