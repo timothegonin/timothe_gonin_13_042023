d505a10123408ddac4de28a305a080678b7764e9
TimotheGonin <gonin.timothe@gmail.com>
2023-05-08 15:00:58 +0200

    🚸 add error + link redirection

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import Error from '../pages/Error'
 
 const Router = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   )
 }
