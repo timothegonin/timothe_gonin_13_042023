b4dc83e522b35f14732df0f893202ea0ca07c6bb
TimotheGonin <gonin.timothe@gmail.com>
2023-05-09 12:16:38 +0200

    💄 little custom for form auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 const SignIn = () => {
   return (
-    <main className="main bg-dark">
+    <main className="main bg-light">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
