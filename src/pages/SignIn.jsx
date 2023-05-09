dd2755f2e8b9c0e718cb0d65a8ba64f5df576cc4
TimotheGonin <gonin.timothe@gmail.com>
2023-05-09 11:44:18 +0200

    🚸 SignIn route + links / fix tags warnings

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,24 +6,18 @@ const SignIn = () => {
         <h1>Sign In</h1>
         <form>
           <div className="input-wrapper">
-            <label for="username">Username</label>
+            <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
           <div className="input-wrapper">
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
-          {/* PLACEHOLDER DUE TO STATIC SITE */}
-          <a href="./user.html" className="sign-in-button">
-            Sign In
-          </a>
-          {/* SHOULD BE THE BUTTON BELOW */}
-          {/* <button className="sign-in-button">Sign In</button> */}
-          {/**/}
+          <button className="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
