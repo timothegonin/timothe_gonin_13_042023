fd447dfb6848b124f27044673ac65aa6e0a9a945
TimotheGonin <gonin.timothe@gmail.com>
2023-05-10 12:20:23 +0200

    🚧 📦 add styled-comp+primary userInfosForm styles

--- src/components/UserInfosForm.jsx
+++ src/components/UserInfosForm.jsx
@@ -1,5 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ STYLES                                                                  │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 13px;
+  input {
+    padding: 10px;
+    font-size: 1.2rem;
+    font-size: 12px;
+    border: 1.5px solid #b8c4ce;
+    border-radius: 2.5px;
+    color: #b8c4ce;
+    width: 167px;
+  }
+`
+
+const ButtonsWrapper = styled.div`
+  margin-top: 9px;
+  display: flex;
+  justify-content: center;
+  gap: 13px;
+  button {
+    width: 101px;
+    height: 35px;
+    border-radius: 2.5px;
+    border: 1.5px solid #a9acea;
+    background-color: #fff;
+    color: #5256ec;
+    font-weight: bold;
+    font-size: 12px;
+    padding: 10px;
+  }
+`
+
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ JSX                                                                     │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
 const UserInfosForm = ({ userInfos, handleToogle, setUserInfos }) => {
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,7 +58,7 @@ const UserInfosForm = ({ userInfos, handleToogle, setUserInfos }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
+      <InputWrapper>
         <input
           type="text"
           placeholder={
@@ -33,11 +77,12 @@ const UserInfosForm = ({ userInfos, handleToogle, setUserInfos }) => {
           onChange={(e) => setUserInfos({ ...userInfos, name: e.target.value })}
           required
         />
-      </div>
-      <div>
+      </InputWrapper>
+
+      <ButtonsWrapper>
         <button>Save</button>
         <button onClick={(e) => handleInput(e)}>Cancel</button>
-      </div>
+      </ButtonsWrapper>
     </form>
   )
 }
