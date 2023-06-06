378ba40655ac0d9b7b148e103f8a2b33cf67b72b
TimotheGonin <gonin.timothe@gmail.com>
2023-06-06 21:10:34 +0200

    💄 ♻️ button style props

--- src/features/userInfosForm/UserInfosView.jsx
+++ src/features/userInfosForm/UserInfosView.jsx
@@ -9,83 +9,20 @@ import { close, open, setUserInfos } from './userInfosFormSlice.js'
   └─────────────────────────────────────────────────────────────────────────┘
  */
 
-// FROM USERINFOSFORM.JSX
-const Form = styled.form`
-  margin-top: 1.2rem;
-`
-
-const InputWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 13px;
-  input {
-    padding: 10px;
-    font-size: 1.2rem;
-    font-size: 12px;
-    border: 1.5px solid #b8c4ce;
-    border-radius: 2.5px;
-    color: #b8c4ce;
-    width: 167px;
-    &::placeholder {
-      color: #b8c4ce;
-    }
-  }
-`
-
-const ButtonsWrapper = styled.div`
-  margin-top: 9px;
-  display: flex;
-  justify-content: center;
-  gap: 13px;
-  button {
-    cursor: pointer;
-    width: 101px;
-    height: 35px;
-    border-radius: 2.5px;
-    border: 1.5px solid #5256ec;
-    background-color: #fff;
-    color: #5256ec;
-    font-weight: bold;
-    font-size: 12px;
-    padding: 10px;
-    position: relative;
-    z-index: 1;
-    &:hover {
-      color: #fff;
-      &::after {
-        opacity: 1;
-      }
-    }
-    &::after {
-      content: '';
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      background: #5256ec;
-      opacity: 0;
-      z-index: -1;
-      transition: opacity 250ms;
-    }
-  }
-`
-
-// FROM USERINFOSFORM.JSX
 const Button = styled.button`
   cursor: pointer;
   width: 101px;
   height: 35px;
   border-radius: 2.5px;
-  border: 1.5px solid #5256ec;
-  background-color: #5256ec;
-  color: #fff;
   font-weight: bold;
   font-size: 12px;
   padding: 10px;
   margin-top: 1.2rem;
   position: relative;
   z-index: 1;
+  border: 1.5px solid #5256ec;
+  background-color: #5256ec;
+  color: #fff;
   &:hover {
     color: #5256ec;
     &::after {
@@ -104,6 +41,46 @@ const Button = styled.button`
     z-index: -1;
     transition: opacity 250ms;
   }
+  ${(props) =>
+    props.$outline &&
+    `margin-top: 0;
+    background-color: #fff;
+    color: #5256ec; 
+    &:hover {
+    color: #fff;
+    }
+    &::after {
+    background: #5256ec;
+    }`}
+`
+
+const Form = styled.form`
+  margin-top: 1.2rem;
+`
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  column-gap: 13px;
+  input {
+    padding: 10px;
+    font-size: 1.2rem;
+    font-size: 12px;
+    border: 1.5px solid #b8c4ce;
+    border-radius: 2.5px;
+    color: #b8c4ce;
+    width: 167px;
+    &::placeholder {
+      color: #b8c4ce;
+    }
+  }
+`
+
+const ButtonsWrapper = styled.div`
+  margin-top: 9px;
+  display: flex;
+  justify-content: center;
+  column-gap: 13px;
 `
 
 /* 
@@ -174,8 +151,10 @@ const UserInfosView = () => {
           </InputWrapper>
 
           <ButtonsWrapper>
-            <button>Save</button>
-            <button onClick={(e) => handleInput(e)}>Cancel</button>
+            <Button $outline>Save</Button>
+            <Button onClick={(e) => handleInput(e)} $outline>
+              Cancel
+            </Button>
           </ButtonsWrapper>
         </Form>
       )}
