68eb26922aa50adeaf1e63b4cf7ea0a147412356
TimotheGonin <gonin.timothe@gmail.com>
2023-06-07 18:37:59 +0200

    🏗️ loginForm=>AuthView+action +auth slice-> store

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/argentBankLogo.png'
 
 const Navbar = () => {
-  const [loggedIn, setLoggedIn] = useState(true)
+  const [loggedIn, setLoggedIn] = useState(false)
 
   const navBarIcon = !loggedIn ? (
     <Link className="main-nav-item" to="/login">
