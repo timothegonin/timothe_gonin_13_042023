8db477b83889fae212c3d514827656849e63d0a6
TimotheGonin <gonin.timothe@gmail.com>
2023-06-09 12:40:31 +0200

    🚸 /profile redirect to /login (user logged out)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Account from '../components/Account'
 import UserInfosView from '../features/userInfosForm/UserInfosView'
 
 const Profile = () => {
+  const navigate = useNavigate()
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login')
+    }
+  }, [isAuthenticated, navigate])
+
   return (
     <main className="main bg-light">
       <div className="header">
