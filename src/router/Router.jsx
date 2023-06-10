4050f15a96bc3d4dfb66b87d91a854e6513378cb
TimotheGonin <gonin.timothe@gmail.com>
2023-06-10 12:35:39 +0200

    🏗️ ♻️ unlogged user redirection to login

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,15 +1,29 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Error from '../pages/Error'
 import Profile from '../pages/Profile'
+
+const PrivateRoute = ({ path, element }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+
+  return isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/login" replace state={{ from: path }} />
+  )
+}
 
 const Router = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/profile"
+        element={<PrivateRoute path="/profile" element={<Profile />} />}
+      />
       <Route path="*" element={<Error />} />
     </Routes>
   )
