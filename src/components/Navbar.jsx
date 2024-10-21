251e13b8cc21ad3420b4bfbf99dc8d16089874e1
timotheG <gonin.timothe@gmail.com>
2024-10-21 15:52:49 +0200

    ♻️💄 navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../features/user/userSlice'
 import logo from '../assets/argentBankLogo.png'
@@ -23,31 +23,46 @@ const Navbar = () => {
    * Renders the appropriate navigation bar icons based on the user's authentication status.
    * @returns {JSX.Element} The rendered navigation bar icons.
    */
+  const linkStyle = 'font-bold text-[#2c3e50] no-underline mr-4 hover:underline'
+  const linkStyleActive = 'text-[#42b983]'
+
   const navBarIcon = !isAuthenticated ? (
-    <Link className="main-nav-item" to="/login">
-      <i className="fa fa-user-circle"></i> Sign In
-    </Link>
+    <NavLink
+      className={({ isActive }) =>
+        isActive ? `${linkStyle} ${linkStyleActive}` : linkStyle
+      }
+      to="/login"
+    >
+      <i className="fa fa-user-circle ml-3 mr-1.5"></i> Sign In
+    </NavLink>
   ) : (
     <React.Fragment>
-      <Link className="main-nav-item" to="/profile">
-        <i className="fa fa-user-circle"></i>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? `${linkStyle} ${linkStyleActive}` : linkStyle
+        }
+        to="/profile"
+      >
+        <i className="fa fa-user-circle  ml-3 mr-1.5"></i>
         {isLoading ? <Loader type="bounce" /> : userFirstName}
-      </Link>
-      <Link className="main-nav-item" to="/" onClick={handleLogout}>
-        <i className="fa fa-sign-out"></i>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? `${linkStyle} ${linkStyleActive}` : linkStyle
+        }
+        to="/"
+        onClick={handleLogout}
+      >
+        <i className="fa fa-sign-out  ml-3 mr-1.5"></i>
         Sign Out
-      </Link>
+      </NavLink>
     </React.Fragment>
   )
 
   return (
-    <nav className="main-nav">
-      <Link className="main-nav-logo" to="/">
-        <img
-          className="main-nav-logo-image"
-          src={logo}
-          alt="Argent Bank Logo"
-        />
+    <nav className="flex justify-between items-center px-5 py-1.5">
+      <Link className="flex items-center" to="/">
+        <img className="max-w-full w-52" src={logo} alt="Argent Bank Logo" />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>{navBarIcon}</div>
