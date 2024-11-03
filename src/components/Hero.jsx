32137484a209decebe132760dc877f18c6988502
timotheG <gonin.timothe@gmail.com>
2024-11-03 17:52:46 +0100

    📱 Hero content + bg adjust

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="bg-[url('/src/assets/bank-tree.jpeg')] bg-cover bg-no-repeat bg-[center_top_-3rem] h-[300px] relative min-[920px]:bg-[center_top_-12rem] min-[920px]:h-[400px]">
-      <section className="hero-content">
+    <div className="bg-[url('/src/assets/bank-tree.jpeg')] bg-cover bg-no-repeat bg-[center_top_-3rem] h-[300px] relative min-[920px]:bg-[center_top_-5.8rem] min-[920px]:h-[400px]">
+      <section className="relative top-8 bg-white p-8 text-left mx-auto my-0 w-64 min-[920px]:absolute min-[920px]:top-14 min-[920px]:right-14 min-[920px]:w-96 min-[920px]:m-8">
         <h2 className="sr-only">Promoted Content</h2>
         <p className="text-base m-0 font-bold leading-5 min-[920px]:text-2xl min-[920px]:leading-6">
           No fees.
