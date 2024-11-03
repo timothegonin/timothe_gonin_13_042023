32bbda88059520603d27671b52839682c89dce44
timotheG <gonin.timothe@gmail.com>
2024-11-03 17:01:16 +0100

    📱 Hero background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="bg-[url('/src/assets/bank-tree.jpeg')] bg-cover bg-no-repeat bg-[center_top_-12rem] h-[400px] relative">
+    <div className="bg-[url('/src/assets/bank-tree.jpeg')] bg-cover bg-no-repeat bg-[center_top_-3rem] h-[300px] relative min-[920px]:bg-[center_top_-12rem] min-[920px]:h-[400px]">
       <section className="hero-content">
         <h2 className="sr-only">Promoted Content</h2>
         <p className="subtitle">No fees.</p>
