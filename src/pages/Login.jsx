0393977e2986b42083ca51f3e7e117425932bd2d
timotheG <gonin.timothe@gmail.com>
2024-11-05 10:31:09 +0100

    💄 sign in final styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,8 +14,11 @@ const Login = () => {
       </Helmet>
       <main className="bg-[#12002b] flex-1">
         <section className="bg-white mx-auto w-80 mt-14 p-8 rounded-md">
-          <i className="fa fa-user-circle sign-in-icon"></i>
-          <h1>Sign In</h1>
+          <i
+            className="fa fa-user-circle sign-in-icon"
+            style={{ fontSize: '20px' }}
+          ></i>
+          <h1 className="mb-5 mt-2 text-2xl font-bold">Sign In</h1>
           <SignIn />
         </section>
       </main>
